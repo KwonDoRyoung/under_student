0281cae50564706ec4480ea7345ddfef545543e4
ABR_Desktop <entropy0437@gmail.com>
2019-01-27 23:07:56 +0900

    행렬 연산 예시

--- abep01/ex_operator.py
+++ abep01/ex_operator.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 if __name__ == "__main__":
     tf.reset_default_graph()
 
+    # 사칙연산
     with tf.device("/device:GPU:0"):
         x = tf.constant(5.0)
         y = tf.constant(6.0)
@@ -19,3 +20,21 @@ if __name__ == "__main__":
         print(sess.run(sub))  # -1.0
         print(sess.run(mul))  # 30.0
         print(sess.run(div))  # 0.8333333
+
+    # 행렬 연산
+    with tf.device("/device:GPU:0"):
+        x = tf.constant([[1.0, 2.0], [5.0, 6.0]])
+        y = tf.eye(2)
+
+        add = tf.add(x, y)  # x + y
+        sub = tf.subtract(x, y)  # x - y
+        mul = tf.multiply(x, y)  # x * y
+        div = tf.div(x, y)  # x / y
+        dot = tf.matmul(x, y)  # x . y
+
+    with tf.Session() as sess:
+        print(sess.run(add))  # [[2. 2.] [5. 7.]]
+        print(sess.run(sub))  # [[0. 2.] [5. 5.]]
+        print(sess.run(mul))  # [[1. 0.] [0. 6.]]
+        print(sess.run(div))  # [[ 1. inf] [inf  6.]]
+        print(sess.run(dot))  # [[1. 2.] [5. 6.]]
